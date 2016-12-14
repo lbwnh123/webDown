8572fb44ccbbc111d78cb712587905a0f05662e3
刘小闻 <liubowen@liuxiaowendeMacBook-Pro.local>
2016-12-14 22:13:06 +0800

    fisrt commit

---
+++ H5down/webNetTextBar/NetworkTextBar.h
@@ -0,0 +1,13 @@
+//
+//  NetworkTextBar.h
+//  H5down
+//
+//  Created by 刘小闻 on 2016/12/14.
+//  Copyright © 2016年 bowenLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NetworkTextBar : UIView
+
+@end
---
+++ H5down/webNetTextBar/NetworkTextBar.m
@@ -0,0 +1,21 @@
+//
+//  NetworkTextBar.m
+//  H5down
+//
+//  Created by 刘小闻 on 2016/12/14.
+//  Copyright © 2016年 bowenLiu. All rights reserved.
+//
+
+#import "NetworkTextBar.h"
+
+@implementation NetworkTextBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
